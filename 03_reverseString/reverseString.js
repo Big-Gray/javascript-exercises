7a59851c3030de2f9102ec935a69ff3cdf6b4bb6
Big-Gray <maximusredman@gmail.com>
2022-06-20 11:33:25 -0400

    Complete exercises 1 and 2.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    res = ""
+    for (let i = s.length-1; i >= 0; i--){
+        res += s.charAt(i);
+    }
+    return res;
 };
 
 // Do not edit below this line
