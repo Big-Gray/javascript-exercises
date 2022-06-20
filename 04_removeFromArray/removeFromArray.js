dd39f9de4de59e55d61ec63fb40c70263d7cbab3
Big-Gray <maximusredman@gmail.com>
2022-06-20 11:48:43 -0400

    Complete exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
+    const blacklist = Array.from(arguments).splice(1);
+    const result = arguments[0].filter(notBlacklisted);
 
+    function notBlacklisted(item) {
+        return !(blacklist.includes(item));
+    }
+    return result;
 };
 
 // Do not edit below this line
