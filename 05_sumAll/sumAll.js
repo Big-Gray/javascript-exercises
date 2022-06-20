87f03410c628f4eda8df02c2e33da149d8ec7247
Big-Gray <maximusredman@gmail.com>
2022-06-20 12:23:07 -0400

    Complete Exercise 5,6, and 7.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof(a) != 'number' || typeof(b) != 'number') return 'ERROR';
+    let hi = Math.max(a,b);
+    let lo = Math.min(a,b);
+    console.log (hi + '' + lo)
+    let sum = 0;
+    while (lo <= hi) {
+        sum += lo;
+        lo++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
